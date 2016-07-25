579f22413798eb595607dd68f6b4378be55b3972
Keenan Johnson <keenan.johnson1@outlook.com>
2016-07-25 12:22:27 -0500

    Write tests for User model, Update User Model and Categories show method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
+  before_save { email.downcase! }
+
+  validates :name, presence:true, length: {maximum: 50}
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: {with: VALID_EMAIL_REGEX}, presence: true, length: {maximum: 255}, uniqueness: {case_sensitive: false}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {User.create(email: 'user@example.com', encrypted_password: 'password123', name: 'XV' )}
+  let(:bad_user) {User.new(email: '', password: 'foobar12', name: 'Jenkins')}
+  #pending "add some examples to (or delete) #{__FILE__}"
+  describe "initialization" do
+  	it "should contain an email, password, and a name" do
+  		expect(user.email).to be_a String
+  		expect(user.encrypted_password).to be_a String
+  		expect(user.name).to be_a String
+  	end
+  	it ".authenticate" do
+  		expect(bad_user.valid?).to be_falsey
+  		expect(bad_user.email).to eq('')
+  		expect(bad_user.password).to eq('foobar12')
+  		expect(bad_user.name).to eq('Jenkins')
+  	end
+  end
 end
